57e413ef4e437b83a2f778276e2577fba13cd82f
AnitaVarsasG <guti.anita88@gmail.com>
2023-05-01 14:24:52 +0200

    HomePage comments and test annotations

--- src/main/java/com/codecool/vizsgaremek/pages/HomePage.java
+++ src/main/java/com/codecool/vizsgaremek/pages/HomePage.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage extends Page{
 
+    //Constructor
     public HomePage(WebDriver driver) {
         super(driver, Pages.URL_HOME.getURL());
     }
 
+    //Locators
     private final By BUTTON_PROFILE = By.id("profile-btn");
     private final By INPUT_NAME = By.id("name");
     private final By INPUT_BIO = By.id("bio");
@@ -17,14 +19,16 @@ public class HomePage extends Page{
     private final By BUTTON_SAVE = By.xpath("//*[@onclick='editUser()']");
     private final By BUTTON_DELETE = By.xpath("//*[@onclick='showRealDeleteAccBtn()']");
     private final By BUTTON_DELETE_CONFIRM = By.id("delete-account-btn");
-
     private final By TEXT_EDIT = By.id("edit-alert");
     private final By BUTTON_LOGOUT = By.id("logout-link");
 
+    //Methods
+    //Navigate to the user profile page
     public void navigateToProfile() {
         driver.findElement(BUTTON_PROFILE).click();
     }
 
+    //Edit the user's profile
     public void editProfile(String name, String bio, String phoneNumber) {
         driver.findElement(INPUT_NAME).sendKeys(name);
         driver.findElement(INPUT_BIO).sendKeys(bio);
@@ -32,15 +36,18 @@ public class HomePage extends Page{
         driver.findElement(BUTTON_SAVE).click();
     }
 
+    //Validate that the user's profile has been edited
     public String validateProfileEdit() {
         return driver.findElement(TEXT_EDIT).getText();
     }
 
+    //Delete an exist account
     public void deleteAccount() {
         driver.findElement(BUTTON_DELETE).click();
         driver.findElement(BUTTON_DELETE_CONFIRM).click();
     }
 
+    //Logout
     public void logout() {
         driver.findElement(BUTTON_LOGOUT).click();
     }
--- src/test/java/com/codecool/vizsgaremek/pages/HomePageTest.java
+++ src/test/java/com/codecool/vizsgaremek/pages/HomePageTest.java
@@ -1,16 +1,19 @@
 package com.codecool.vizsgaremek.pages;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
+@Story("User profile modifications")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HomePageTest {
 
     WebDriver driver;
@@ -53,44 +56,66 @@ class HomePageTest {
     }
 
     @Test
+    @DisplayName("TC08 - Test edit a user's profile")
+    @Order(1)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify the ability to edit profile information")
     void editProfileTest() {
 
-        //test data for edit profile
+        // Test data for edit profile
         String name = "Test Edit";
         String bio = "test bio";
         String phoneNumber = "02345678";
 
+        // Register and login user
         regAndLogin();
 
+        // Navigate to profile and edit profile information
         homePage.navigateToProfile();
         homePage.editProfile(name, bio, phoneNumber);
 
         String actualResult = homePage.validateProfileEdit();
         String expectedResult = "Profile Edited!";
 
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedResult, actualResult, "Profile edit success message is not matching!");
     }
 
     @Test
+    @DisplayName("TC09 - Test for logging out")
+    @Order(2)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify that logging out and redirecting to the landing page")
     void logoutTest() {
+
+        // Register and login user
         regAndLogin();
 
+        // Logout
         homePage.logout();
         String expectedResult = "https://lennertamas.github.io/roxo/index.html";
         String actualResult = driver.getCurrentUrl();
 
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedResult, actualResult, "The actual URL does not match the expected URL after logout");
     }
 
     @Test
+    @DisplayName("TC10 - Test delete an exist account")
+    @Order(3)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("This test case verifies if a user can successfully delete their account and the account is no longer accessible afterwards.")
     void deleteAccountTest() {
+
+        // Register and login user
         regAndLogin();
 
+        // Delete user's account
         homePage.navigateToProfile();
         homePage.deleteAccount();
+
+        // Try to log in with the deleted account
         registerAndLogin.login(user, pwd);
 
-        Assertions.assertTrue(registerAndLogin.loginFailedValidation());
+        Assertions.assertTrue(registerAndLogin.loginFailedValidation(), "The account was not successfully deleted or still accessible.");
     }
 
     @AfterEach
